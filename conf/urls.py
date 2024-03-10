b696f9ed639689bf7043ba679012ef62fe7f631f
Elyorbek Babayev <159513285+elyorbek0414@users.noreply.github.com>
2024-03-10 12:54:54 +0500

    telegram_bot commit

--- conf/urls.py
+++ conf/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('open_weather_bot/v1/', include('open_weather_bot.urls'))
+    path('open_weather_bot/v1/', include('open_weather_bot.urls')),
 ]
